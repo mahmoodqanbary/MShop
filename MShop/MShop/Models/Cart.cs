b8d99a9c73f0d6a36e78cd900971debc26b4052a
Mahmood Qanbary <MahmoodQanbary@gmail.com>
2021-04-02 11:20:19 -0700

    Cart model complete

--- MShop/MShop/Models/Cart.cs
+++ MShop/MShop/Models/Cart.cs
@@ -7,5 +7,38 @@ namespace MShop.Models
 {
     public class Cart
     {
+        public Cart()
+        {
+            CartItems = new List<CartItem>();
+        }
+        public int OrderId { get; set; }
+        public List<CartItem> CartItems { get; set; }
+
+        public void addItem(CartItem item)
+        {
+            if(CartItems.Exists(i => i.Item.Id == item.Id))
+            {
+                CartItems.Find(i => i.Item.Id == item.Item.Id)
+                    .Quantity += 1;
+            }
+            else
+            {
+                CartItems.Add(item);
+            }
+        }
+
+        public void removeItem(int itmeId)
+        {
+            var item = CartItems.SingleOrDefault(c => c.Item.Id == itmeId);
+            if(item?.Quantity <= 1)
+            {
+                CartItems.Remove(item);
+            }
+            else if(item != null)
+            {
+                item.Quantity -= 1;
+            }
+        }
+
     }
 }

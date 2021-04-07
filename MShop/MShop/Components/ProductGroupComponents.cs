59073bd4d67610fcc2024bbcd249f3e14e1e5e67
Mahmood Qanbary <MahmoodQanbary@gmail.com>
2021-04-07 12:30:08 -0700

    View Components

---
+++ MShop/MShop/Components/ProductGroupComponents.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MShop.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MShop.Components
+{
+    public class ProductGroupComponents : ViewComponent
+    {
+        private MShopDbContext _context;
+        public ProductGroupComponents(MShopDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View("/Views/Components/ProductGroupsComponent.cshtml", _context.Categories);
+        }
+    }
+}
